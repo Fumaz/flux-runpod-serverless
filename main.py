9cd2951e07c85e45f65150f1baaf9204367c86e1
Fumaz <mail@fumaz.dev>
2024-08-11 14:14:13 +0200

    fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from io import BytesIO
 
 import runpod
 import torch
+from PIL import Image
 from diffusers import FluxPipeline
 
 pipe = FluxPipeline.from_pretrained(os.environ.get("FLUX_MODEL_NAME", "black-forest-labs/FLUX.1-dev"),
@@ -16,13 +17,32 @@ pipe = pipe.to("cuda")
 def handler(job):
     job_input = job["input"]
     prompt = job_input["prompt"]
-    height = job_input["height"]
-    width = job_input["width"]
     guidance_scale = job_input["guidance_scale"]
     num_inference_steps = job_input["num_inference_steps"]
 
+    init_image_str = job_input.get("init_image")
+
+    if init_image_str:
+        init_image = Image.open(BytesIO(base64.b64decode(init_image_str)))
+        aspect_ratio = init_image.width / init_image.height
+
+        max_size = job_input.get("max_size", 2048)
+        if aspect_ratio > 1:
+            width = min(init_image.width, max_size)
+            height = int(width / aspect_ratio)
+        else:
+            height = min(init_image.height, max_size)
+            width = int(height * aspect_ratio)
+
+        init_image = init_image.resize((width, height))
+    else:
+        width = job_input.get("width", 1024)
+        height = job_input.get("height", 1024)
+        init_image = None
+
     image = pipe(
         prompt=prompt,
+        image=init_image,
         height=height,
         width=width,
         guidance_scale=guidance_scale,
